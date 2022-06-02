e53bccbfe7886cd4ff1072200ea81c265cd76de0
Amrega <rtpamrega@gmail.com>
2022-06-02 13:51:34 +0700

    add db

--- backend/pemrograman_integratif.sql
+++ backend/pemrograman_integratif.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 19 Apr 2022 pada 03.30
--- Versi server: 10.4.21-MariaDB
--- Versi PHP: 8.0.12
+-- Generation Time: Jun 02, 2022 at 08:50 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `account`
+-- Table structure for table `account`
 --
 
 CREATE TABLE `account` (
@@ -38,7 +38,7 @@ CREATE TABLE `account` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `account`
+-- Dumping data for table `account`
 --
 
 INSERT INTO `account` (`account_id`, `account_username`, `account_password`, `account_pin`, `nomer_hp`, `account_money`, `account_role`) VALUES
@@ -54,7 +54,38 @@ INSERT INTO `account` (`account_id`, `account_username`, `account_password`, `ac
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `detail_transaction`
+-- Table structure for table `account_m`
+--
+
+CREATE TABLE `account_m` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buy_m`
+--
+
+CREATE TABLE `buy_m` (
+  `id` int(11) NOT NULL,
+  `id_buyer` int(11) NOT NULL,
+  `id_seller` int(11) NOT NULL,
+  `id_item` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaction`
 --
 
 CREATE TABLE `detail_transaction` (
@@ -65,7 +96,7 @@ CREATE TABLE `detail_transaction` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `detail_transaction`
+-- Dumping data for table `detail_transaction`
 --
 
 INSERT INTO `detail_transaction` (`detail_id`, `transaction_id`, `detail_item`, `detail_price`) VALUES
@@ -76,7 +107,21 @@ INSERT INTO `detail_transaction` (`detail_id`, `transaction_id`, `detail_item`, 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `transaction`
+-- Table structure for table `item_m`
+--
+
+CREATE TABLE `item_m` (
+  `id` int(11) NOT NULL,
+  `itemname` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
 --
 
 CREATE TABLE `transaction` (
@@ -88,7 +133,7 @@ CREATE TABLE `transaction` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `transaction`
+-- Dumping data for table `transaction`
 --
 
 INSERT INTO `transaction` (`transaction_id`, `account_id`, `transaction_marketplace`, `transaction_timedate`, `transaction_totalprice`) VALUES
@@ -100,7 +145,7 @@ INSERT INTO `transaction` (`transaction_id`, `account_id`, `transaction_marketpl
 --
 
 --
--- Indeks untuk tabel `account`
+-- Indexes for table `account`
 --
 ALTER TABLE `account`
   ADD PRIMARY KEY (`account_id`),
@@ -110,53 +155,89 @@ ALTER TABLE `account`
   ADD UNIQUE KEY `nomer_hp` (`nomer_hp`);
 
 --
--- Indeks untuk tabel `detail_transaction`
+-- Indexes for table `account_m`
+--
+ALTER TABLE `account_m`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `buy_m`
+--
+ALTER TABLE `buy_m`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `detail_transaction`
 --
 ALTER TABLE `detail_transaction`
   ADD PRIMARY KEY (`detail_id`),
   ADD KEY `transcation_id` (`transaction_id`);
 
 --
--- Indeks untuk tabel `transaction`
+-- Indexes for table `item_m`
+--
+ALTER TABLE `item_m`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaction`
 --
 ALTER TABLE `transaction`
   ADD PRIMARY KEY (`transaction_id`),
   ADD KEY `account_id` (`account_id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `account`
+-- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
   MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT untuk tabel `detail_transaction`
+-- AUTO_INCREMENT for table `account_m`
+--
+ALTER TABLE `account_m`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `buy_m`
+--
+ALTER TABLE `buy_m`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `detail_transaction`
 --
 ALTER TABLE `detail_transaction`
   MODIFY `detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `transaction`
+-- AUTO_INCREMENT for table `item_m`
+--
+ALTER TABLE `item_m`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
   MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `detail_transaction`
+-- Constraints for table `detail_transaction`
 --
 ALTER TABLE `detail_transaction`
   ADD CONSTRAINT `transcation_id` FOREIGN KEY (`transaction_id`) REFERENCES `transaction` (`transaction_id`);
 
 --
--- Ketidakleluasaan untuk tabel `transaction`
+-- Constraints for table `transaction`
 --
 ALTER TABLE `transaction`
   ADD CONSTRAINT `account_id` FOREIGN KEY (`account_id`) REFERENCES `account` (`account_id`);
