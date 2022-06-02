5cf0a70eaf7918b61bcc3586bb712ff0e47df2f5
Amrega <rtpamrega@gmail.com>
2022-06-03 00:22:54 +0700

    add sedikit

--- app/Controllers/buskimarket/Buy.php
+++ app/Controllers/buskimarket/Buy.php
@@ -14,6 +14,29 @@ class Home extends BaseController
     {
         $this->ModelBuy_m = new ModelBuy_m();
         helper('jwt');
+
+        $rules = [
+            "id_item" => [
+                'label'     => 'id_item',
+                'rules'     => 'required',
+                'errors'    => [
+                    'required'  => 'silahkan masukkan id_item'
+                ]
+            ],
+            "emoney" => [
+                'label'     => 'emoney',
+                'rules'     => 'required',
+                'errors'    => [
+                    'required'  => 'silahkan masukkan emoney'
+                ]
+            ],
+        ];
+        if (!$this->validate($rules)) {
+            $validation = \Config\Services::validation();
+            return $this->fail($validation->getErrors());
+        }
+        $data_jwt = getJWTdata($this->request->getHeader("Authorization")->getValue());
+
         
     }
 }
